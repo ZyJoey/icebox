a7ab72d63db25a6da67606076d9a99b7f439f65a
joey11 <zhouyi0112@126.com>
2016-04-19 21:13:54 +0800

    add button in list

--- components/component/list/list.js
+++ components/component/list/list.js
@@ -1,19 +1,20 @@
 var baseInfo = require('common/baseInfo');
-require("common/dialog");
+var util = require('common/util');
+var dialog = require("common/dialog");
 
 module.exports = Vue.extend({
 	inherit:true,
 	template:__inline("list.html"),
 	data:function(){
-		var i,
-		result,
-		sortord = ["按即将过期时间排序","按放入时间排序(正序)","按放入时间排序(倒序)"],
-		select = {
-			"sortord":sortord[0]
-		},
-		listOrder = 'deadline',
-		sortKey = 1;
-		type = this.$route.params.type;
+		var	result,
+			sortord = ["按即将过期时间排序","按放入时间排序(正序)","按放入时间排序(倒序)"],
+			select = {
+				"sortord":sortord[0]
+			},
+			listOrder = 'deadline',
+			sortKey = 1;
+			type = this.$route.params.type;
+
 		type = type === "all" ? "" : type;
 		if(!this.$parent.list){
 			baseInfo.getFoodlist(this);
@@ -24,7 +25,8 @@ module.exports = Vue.extend({
 			select:select,
 			listOrder:listOrder,
 			sortKey:sortKey,
-			type:type
+			type:type,
+			btnW : new Array(this.$parent.list.length)
 		}
 	},
 	methods:{
@@ -53,6 +55,38 @@ module.exports = Vue.extend({
 				this.showSelect = false;
 			}
 			return false;
+		},
+		showBtn:function(index,e){
+			var 	i,
+				that = this;
+			move(e.target,e,function(x,y){
+				for(i = 0 ;i < that.btnW.length ;i++ ){
+					if(i == index){
+						continue;
+					} 
+					that.btnW.$set([i],0);
+				}
+				if(x < 0 && x > -30){
+					that.btnW.$set([index],-2*x);
+				}else if(x <= -30){
+					that.btnW.$set([index],60);
+				}else{
+					that.btnW.$set([index],0);
+				}
+			});
+		},
+		removeFood:function(index,id){
+			var that = this;
+			this.btnW.$set([index],0);
+			this.$http.delete('server/delFood',{"id":id}).then(function (data){
+				if(data.data.code === 0){
+					baseInfo.getFoodlist(that);
+
+					dialog.info({content:data.data.msg});
+				}else{
+					dialog.info({content:data.data.msg});
+				}
+			})
 		}
 	}
 
